4ecda4f72af855aa43aa60b89474114ea75842a7
Estevão <estevao.biondi@gmail.com>
2022-11-12 10:32:07 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,7 @@ import "reflect-metadata";
 
 import "@shared/container";
 
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
@@ -24,6 +25,11 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(router);
 
 app.use(
